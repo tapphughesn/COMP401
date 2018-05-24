baef8f4f6ba9d91d11f51373c129d28bcff54e5a
Nicholas Tapp-Hughes <nic98@live.unc.edu>
2018-05-24 18:57:02 -0400

    Create Sushi.java

---
+++ COMP401.Sushi/Sushi.java
@@ -0,0 +1,14 @@
+package comp401.sushi;
+
+public interface Sushi {
+
+	String getName();
+	IngredientPortion[] getIngredients();
+	int getCalories();
+	double getCost();
+	boolean getHasRice();
+	boolean getHasShellfish();
+	boolean getIsVegetarian();
+	Plate.SushiType getSushiType();
+	
+}
